7e68d0b49c3fe8309cf33b41f9817db209a3c473
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2024-11-06 10:22:51 +0800

    swiper

--- src/components/layout/home/TeamSection.tsx
+++ src/components/layout/home/TeamSection.tsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Box, Grid, Paper, Typography, Collapse, Button } from "@mui/material";
+import { Box, Grid, Paper, Typography, Button, Tab, Tabs } from "@mui/material";
 import { useThemeContext } from "@/theme/themeProvider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Default Swiper CSS
+import "swiper/css/navigation"; // If you're using navigation
+import "swiper/css/pagination"; // If you're using pagination
+import "swiper/css/scrollbar";
+import SwiperCore from "swiper"; // Import Scrollbar from SwiperCore
+import { Scrollbar } from "swiper/modules";
+import { useTheme } from "@mui/material/styles";
+
+// Install the module in SwiperCore
 
 type CustomTheme = {
   activeSet: number; // Adjust this based on your actual structure
@@ -60,63 +70,67 @@ const teamMembers: TeamMember[] = [
 
 const teamModerators: TeamMember[] = [
   {
-    name: "Squidicuz",
-    role: "Leads",
+    name: "ANormalStick",
+    role: "Moderator",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "John has over 10 years of experience in the industry and leads our team with vision and strategy.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : anormalstick",
   },
   {
-    name: "SeqSee",
-    role: "Leads",
+    name: "Ferbog05",
+    role: "Moderator",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "Jane is the brain behind our technology and oversees all tech developments.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : ferbog05",
   },
   {
-    name: "Vanikoro",
-    role: "Leads",
+    name: "Matador",
+    role: "Moderator",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "Alice brings creativity and innovation to our projects, ensuring a great user experience.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : matador",
   },
   {
-    name: "Kyuhi",
-    role: "Leads",
+    name: "Demil",
+    role: "Moderator",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "Bob is a full-stack developer who loves turning ideas into reality through code.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : demil",
   },
   {
-    name: "Shikoku",
-    role: "Leads",
+    name: "DeathSmack",
+    role: "Moderator",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "Bob is a full-stack developer who loves turning ideas into reality through code.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : deathsmack",
   },
   {
-    name: "Shikoku",
-    role: "Leads",
+    name: "ANormalTonic",
+    role: "Contributor",
     image: "/static/images/green-head.webp",
     land: "/static/images/green-land.webp",
-    details:
-      "Bob is a full-stack developer who loves turning ideas into reality through code.",
-    dcdetails: "Discord : squidicuz",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : anormaltonic",
+  },
+  {
+    name: "Ercknard",
+    role: "Contributor",
+    image: "/static/images/green-head.webp",
+    land: "/static/images/green-land.webp",
+    details: "Welcome to CryptechTest",
+    dcdetails: "Discord : ercknard",
   },
 ];
 
 const TeamSection: React.FC = () => {
   const { activeSet } = useThemeContext() as CustomTheme;
+  const [activeTab, setActiveTab] = useState<number>(0); // 0 for Team Members, 1 for Moderators
+  const theme = useTheme();
 
   // Ensure activeSet is a number for comparison purposes
   const activeSetNumber =
@@ -181,6 +195,8 @@ const TeamSection: React.FC = () => {
   const imageBgBannerSrc =
     colorSetBgBannerRight[activeSet.toString()] || colorSetBgBannerRight[1];
 
+  SwiperCore.use([Scrollbar]);
+
   return (
     <Box
       position={"absolute"}
@@ -228,6 +244,16 @@ const TeamSection: React.FC = () => {
         <Typography variant="h4" align="center" gutterBottom>
           Meet Our Team
         </Typography>
+
+        <Tabs
+          value={activeTab}
+          onChange={(_, newValue) => setActiveTab(newValue)}
+          centered
+          sx={{ marginTop: 2 }}
+        >
+          <Tab label="Leads" />
+          <Tab label="Moderators" />
+        </Tabs>
 
         <Box position={"relative"}>
           {/* Expanded Details Section */}
@@ -304,83 +330,16 @@ const TeamSection: React.FC = () => {
               </Typography>
             </Box>
           )}
-
-          <Grid
-            position={"relative"}
-            zIndex={2}
-            container
-            spacing={3}
-            justifyContent="center"
-          >
-            {teamMembers.map((member) => (
-              <Grid item xs={12} sm={6} md={2.4} key={member.name}>
-                <Paper
-                  elevation={3}
-                  sx={{
-                    padding: 2,
-                    textAlign: "center",
-                    transition: "transform 0.2s, background-color 0.3s",
-                    cursor: expandedMember === member ? "pointer" : "pointer", // Disable pointer cursor for active card
-                    backgroundColor:
-                      expandedMember === member
-                        ? "custom.secondarySolidColors"
-                        : "custom.secondaryBackgroundGrayed", // Change color when active
-                    "&:hover": {
-                      transform:
-                        expandedMember === member ? "none" : "scale(1.05)", // Disable hover effect for active card
-                      backgroundColor:
-                        expandedMember === member
-                          ? "custom.secondarySolidColors"
-                          : "custom.primaryComponents", // Optional hover effect
-                    },
-                  }}
-                  onClick={() => handleCardClick(member)}
-                >
-                  <Box
-                    component={"img"}
-                    src={member.image}
-                    alt={`${member.name} - ${member.role}`}
-                    sx={{
-                      width: "100%",
-                      height: "auto",
-                      borderRadius: "50%",
-                    }}
-                  />
-                  <Typography
-                    variant="body1"
-                    fontSize={"1.25rem"}
-                    sx={{ marginTop: 2 }}
-                  >
-                    {member.name}
-                  </Typography>
-                  <Typography variant="h5">{member.role}</Typography>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
-
-          {/* Button to toggle collapse of moderators */}
-          {/* <Button
-            onClick={() => setShowModerators((prev) => !prev)}
-            sx={{ marginTop: 2 }}
-            variant="contained"
-            color="primary"
-          >
-            {showModerators ? "Hide Moderators" : "Show Moderators"}
-          </Button> */}
-
-          {/* Collapsing section for moderators */}
-          {/* <Collapse in={showModerators}>
+          {activeTab === 0 && (
             <Grid
               position={"relative"}
               zIndex={2}
               container
               spacing={3}
               justifyContent="center"
-              marginTop={0.25}
             >
-              {teamModerators.map((member) => (
-                <Grid item xs={12} sm={6} md={2} key={member.name}>
+              {teamMembers.map((member) => (
+                <Grid item xs={12} sm={6} md={2.4} key={member.name}>
                   <Paper
                     elevation={3}
                     sx={{
@@ -413,19 +372,120 @@ const TeamSection: React.FC = () => {
                         borderRadius: "50%",
                       }}
                     />
-                    <Typography variant="h6" sx={{ marginTop: 2 }}>
+                    <Typography
+                      variant="body1"
+                      fontSize={"1.25rem"}
+                      sx={{ marginTop: 2 }}
+                    >
                       {member.name}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {member.role}
-                    </Typography>
+                    <Typography variant="h5">{member.role}</Typography>
                   </Paper>
                 </Grid>
               ))}
             </Grid>
-          </Collapse> */}
+          )}
+
+          {activeTab === 1 && (
+            <Box>
+              <Swiper
+                spaceBetween={23} // Space between slides
+                slidesPerView={5} // Show 5 slides at a time
+                loop={false} // Enable looping of the slides
+                autoplay={{
+                  delay: 2500, // Autoplay delay (in ms)
+                  disableOnInteraction: false, // Keep autoplay on after interaction
+                }}
+                scrollbar={{
+                  hide: false, // Keep scrollbar visible
+                  draggable: true, // Make the scrollbar draggable
+                }}
+                modules={[Scrollbar]}
+                breakpoints={{
+                  1024: {
+                    slidesPerView: 5, // For large screens (desktops)
+                  },
+                  768: {
+                    slidesPerView: 3, // For tablets (portrait mode)
+                  },
+                  480: {
+                    slidesPerView: 1, // For small screens (mobile devices)
+                  },
+                }}
+              >
+                {teamModerators.map((member) => (
+                  <SwiperSlide key={member.name}>
+                    <Paper
+                      elevation={3}
+                      sx={{
+                        padding: 2,
+                        textAlign: "center",
+                        backgroundColor:
+                          expandedMember === member
+                            ? "custom.secondarySolidColors"
+                            : "custom.secondaryBackgroundGrayed",
+                        transition: "transform 0.2s, background-color 0.3s",
+                        cursor: "pointer",
+                        "&:hover": {
+                          transform: "scale(1.05)",
+                          backgroundColor:
+                            expandedMember === member
+                              ? "custom.secondarySolidColors"
+                              : "custom.primaryComponents",
+                        },
+                      }}
+                      onClick={() => handleCardClick(member)}
+                    >
+                      <Box
+                        component={"img"}
+                        src={member.image}
+                        alt={`${member.name} - ${member.role}`}
+                        sx={{
+                          width: "100%",
+                          height: "auto",
+                          borderRadius: "50%",
+                        }}
+                      />
+                      <Typography
+                        variant="body1"
+                        fontSize={"1.25rem"}
+                        sx={{ marginTop: 2 }}
+                      >
+                        {member.name}
+                      </Typography>
+                      <Typography variant="h5">{member.role}</Typography>
+                    </Paper>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </Box>
+          )}
         </Box>
       </Box>
+
+      <style jsx global>
+        {`
+          /* Swiper scrollbar customizations */
+          .swiper-scrollbar {
+            background-color: ${theme.palette.custom
+              .secondaryBackground}; /* Track background */
+            border-radius: 10px; /* Rounded track edges */
+            height: 8px; /* Scrollbar height */
+          }
+
+          .swiper-scrollbar-drag {
+            background-color: ${theme.palette.custom
+              .mainColor}; /* Thumb color */
+            border-radius: 10px; /* Rounded thumb */
+            opacity: 0.7; /* Thumb opacity */
+          }
+
+          .swiper-scrollbar-drag:hover {
+            background-color: ${theme.palette.custom
+              .secondarySolidColors}; /* Thumb color on hover */
+          }
+        `}
+      </style>
     </Box>
   );
 };
